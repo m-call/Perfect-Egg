b7a135ee7926cda274cf0dbbd6c1fd6b8b9e4190
Michael Callahan <mcallahan0@yahoo.com>
2022-01-23 12:57:38 -0500

    Changed secondsRemaining to secondsPassed and created variable for total time of timer. Changed conditional in updateTimer() to increment secondsPassed and then update the progress bar with the secondsPassed divided by the totalTime to show how much progress has been made. Finally, set the titleLabel to the hardness selected and set the progress bar back to 0 and secondsPassed to 0 at the top of the IBAction so that it will reset each time a button is clicked.

--- Perfect Egg/ViewController.swift
+++ Perfect Egg/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
     
     let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
     
-    var secondsRemaining = 60
-    
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
@@ -23,15 +24,19 @@ class ViewController: UIViewController {
         
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             titleLabel.text = "Done!"
