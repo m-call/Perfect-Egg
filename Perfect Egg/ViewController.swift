ca14e45740751768357e63299a51c13c7eb07059
Michael Callahan <mcallahan0@yahoo.com>
2022-01-23 11:44:43 -0500

    Rearranged Button and Image Views in Main Storyboard. Created IBAction function linked to each of the 3 buttons. Created constants for egg timer. Set up basic conditional for egg that is selected.

--- Perfect Egg/ViewController.swift
+++ Perfect Egg/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        if (hardness == "soft") {
+            print(softTime)
+        } else if (hardness == "medium") {
+            print(mediumTime)
+        } else {
+            print(hardTime)
+        }
     }
-
-
+    
 }
 
