0910d64dd45d03b3bff35db10717bef008033205
Michael Callahan <mcallahan0@yahoo.com>
2022-01-23 12:31:00 -0500

    Created constant dictionary for egg timer values. Created var for seconds remaining in timer. Created updateTimer function to count down and decrement the secondsRemaining variable. Used Timer class and scheduledTimer method to create the timer.

--- Perfect Egg/ViewController.swift
+++ Perfect Egg/ViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    
+    var secondsRemaining = 60
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
         
-        if (hardness == "soft") {
-            print(softTime)
-        } else if (hardness == "medium") {
-            print(mediumTime)
-        } else {
-            print(hardTime)
+        secondsRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
         }
     }
     
