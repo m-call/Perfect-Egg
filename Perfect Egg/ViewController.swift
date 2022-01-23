68bbc86f1513c241c8527da956e50b43b0cde2a6
Michael Callahan <mcallahan0@yahoo.com>
2022-01-23 12:38:16 -0500

    Added IBOutlet for the title label so that it's value can be changed to Done when the timer hits 0. Added else condition to the conditional in updateTimer() where timer is invalidated and the titleLabel text property is changed.

--- Perfect Egg/ViewController.swift
+++ Perfect Egg/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
     
     var secondsRemaining = 60
@@ -30,6 +32,9 @@ class ViewController: UIViewController {
         if secondsRemaining > 0 {
             print("\(secondsRemaining) seconds.")
             secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
         }
     }
     
